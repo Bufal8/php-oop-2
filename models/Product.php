69a0d9d42eeaebcac12a458dd9c70224761f182b
Manuel Floriani <manuel.floriani1999@gmail.com>
2023-07-03 15:41:16 +0200

    Creazione modello category

---
+++ models/Product.php
@@ -0,0 +1,63 @@
+<?php
+
+class Product {
+
+    private $name;
+    private $image;
+    private $price;
+    private $categoryIcon;
+    private $type;
+
+    public function __construct($name, $image, $price, $categoryIcon, $type) {
+
+        $this -> setName($name);
+        $this -> setImage($image);
+        $this -> setPrice($price);
+        $this -> setCategoryIcon($categoryIcon);
+        $this -> setType($type);
+    }
+
+    public function getName() {
+
+        return $this -> name;
+    }
+    public function setName($name) {
+
+        $this -> name = $name;
+    }
+
+    public function getImage() {
+
+        return $this -> image;
+    }
+    public function setImage($image) {
+
+        $this -> image = $image;
+    }
+
+    public function getPrice() {
+
+        return $this -> price;
+    }
+    public function setPrice($price) {
+
+        $this -> price = $price;
+    }
+
+    public function getCategoryIcon() {
+
+        return $this -> categoryicon;
+    }
+    public function setCategoryIcon($categoryicon) {
+
+        $this -> categoryicon = $categoryicon;
+    }
+    public function getType() {
+
+        return $this -> type;
+    }
+    public function setType($type) {
+
+        $this -> type = $type;
+    }
+}
